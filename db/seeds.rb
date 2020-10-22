17f2bc3eb07210fff1fce0db6802dfd1a63b647a
Michele Comolli <mikicomolli@gmail.com>
2020-10-22 10:54:16 +0200

    Saving Reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+restaurant = Restaurant.create([
+  { name: 'Gino', address: 'Milano', phone_number: '234567897654', category: 'italian' },
+  { name: 'Julien', address: 'Parigi', phone_number: '234567897654', category: 'french' },
+  { name: 'Friedrich', address: 'Belgio', phone_number: '2345567897654', category: 'belgian' },
+  { name: 'Mao', address: 'Shangai', phone_number: '9876567897654', category: 'chinese' },
+  { name: 'Natasha', address: 'Tokyo', phone_number: '234567897654', category: 'japanese' }
+  ])
